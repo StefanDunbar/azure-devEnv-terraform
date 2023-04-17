970721041d43f9cd41b00c23d5856285e17e4a7b
Stefan Dunbar <stefanddunbar@googlemail.com>
2023-04-17 23:29:48 +0100

    update to main.tf  variable entry

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "azurerm_network_security_rule" "aztf-dev-rule" {
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefix       = var.personal_ip #Your IP declared as variable in variables.tf
+  source_address_prefix       = "${var.personal_ip}" #Your IP declared as variable in variables.tf
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.aztf-rg.name
   network_security_group_name = azurerm_network_security_group.aztf-nsg.name
